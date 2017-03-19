f7f0db62ad7ffb7d42ab3acb1a28aafb17d3baa1
Artyom Tukanov <artemtukanow@gmail.com>
2017-03-19 16:50:35 +0300

    Добавили список новостей

--- Assets/Scripts/Layers/ShopLayer.cs
+++ Assets/Scripts/Layers/ShopLayer.cs
@@ -1,37 +1,7 @@
-﻿using Assets.Scripts.Layers.Enum;
-using UnityEngine;
-
-namespace Assets.Scripts.Layers
+﻿namespace Assets.Scripts.Layers
 {
-    public class ShopLayer : ILayerBase
+    public class ShopLayer
     {
-        private GameObject _canvas = GameObject.Find("shopCanvas");
-        private LayerNamesEnum _name = LayerNamesEnum.shops;
 
-        public LayerNamesEnum Name { get { return _name; } }
-
-        public GameObject Canvas { get { return _canvas; }}
-
-        public static ShopLayer instance = new ShopLayer();
-
-        private ShopLayer()
-        {
-
-        }
-
-        public void OnVisable()
-        {
-
-        }
-
-        public void OnDisable()
-        {
-
-        }
-
-        public void OnReload()
-        {
-
-        }
     }
 }
---
+++ Assets/Scripts/Layers/ShopsLayer.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.Layers.Enum;
+using UnityEngine;
+
+namespace Assets.Scripts.Layers
+{
+    public class ShopLayer : ILayerBase
+    {
+        private GameObject _canvas = GameObject.Find("shopCanvas");
+        private LayerNamesEnum _name = LayerNamesEnum.shops;
+
+        public LayerNamesEnum Name { get { return _name; } }
+
+        public GameObject Canvas { get { return _canvas; }}
+
+        public static ShopLayer instance = new ShopLayer();
+
+        private ShopLayer()
+        {
+
+        }
+
+        public void OnVisable()
+        {
+
+        }
+
+        public void OnDisable()
+        {
+
+        }
+
+        public void OnReload()
+        {
+
+        }
+    }
+}
